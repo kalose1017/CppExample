c67f3f46e2bfeb95080866d71e26de46531e13c2
kalose1017 <leejiho1369@naver.com>
2025-05-29 10:32:26 +0900

    0529 C++ 실습 예제 업데이트

---
+++ 250529Exam/cinGetExam/cinGetExam.cpp
@@ -0,0 +1,20 @@
+﻿#include <iostream>
+
+int textin4()
+{
+	int ch;
+	int count = 0;
+
+	while ((ch = std::cin.get()) != EOF)
+	{
+		std::cout.put(ch);
+		++count;
+	}
+	std::cout << count << "문자를 읽었습니다." << std::endl;
+	return 0;
+}
+
+int main()
+{
+	textin4();
+}
